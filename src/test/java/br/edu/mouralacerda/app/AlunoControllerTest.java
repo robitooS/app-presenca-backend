afe5704d29b4d9326ff8658f67b98d93037375ac
Higor Robin <proghigorrobin@gmail.com>
2025-06-08 22:24:46 -0300

    Arquivo de teste para a classe AlunoController

--- src/test/java/br/edu/mouralacerda/app/AlunoControllerTest.java
+++ src/test/java/br/edu/mouralacerda/app/AlunoControllerTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest // <-- ANOTAÇÃO ESSENCIAL QUE ESTAVA FALTANDO
 @AutoConfigureMockMvc
 public class AlunoControllerTest {
 
